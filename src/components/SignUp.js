d8226cffb8a79da069f66096907fc6a5c872ce6d
jwaltrip <jake.waltrip@gmail.com>
2018-09-16 06:25:49 -0500

    successfully adding user to firebase, but cannot redirect

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,8 +1,131 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  Link,
+  withRouter,
+} from 'react-router-dom';
+import { auth } from '../firebase';
 
-const SignUpPage = () =>
-  <div>
-    <h1>Sign Up Page</h1>
-  </div>;
+// import route constants
+import * as routes from '../constants/routes';
 
-export default SignUpPage;
+// sign up form page component
+const SignUpPage = ({ history }) => {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <SignUpForm history={history} />
+    </div>
+  );
+};
+
+const INITIAL_STATE = {
+  username: '',
+  email: '',
+  passwordOne: '',
+  passwordTwo: '',
+  error: null,
+};
+
+const byPropKey = (propertyName, value) => () => ({
+  [propertyName]: value,
+});
+
+// sign up form
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {...INITIAL_STATE};
+  }
+
+  onSubmit = (e) => {
+    const {
+      username,
+      email,
+      passwordOne,
+    } = this.state;
+
+    const {
+      history,
+    } = this.props;
+
+    auth.doCreateUserWithEmailAndPassword(email, passwordOne)
+      .then(authUser => {
+        this.setState({ ...INITIAL_STATE });
+        this.props.history.push(routes.SIGN_IN);
+      })
+      .catch(error => {
+        // console.log('error creating user', error);
+        this.setState(byPropKey('error', error ));
+      });
+
+    e.preventDefault();
+  };
+
+  render() {
+    const {
+      username,
+      email,
+      passwordOne,
+      passwordTwo,
+      error,
+    } = this.state;
+
+    const isInvalid =
+      passwordOne !== passwordTwo ||
+      passwordOne === '' ||
+      email === '' ||
+      username === '';
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          value={username}
+          onChange={event => this.setState(byPropKey('username', event.target.value))}
+          type="text"
+          placeholder="Username"
+        />
+        <input
+          value={email}
+          onChange={event => this.setState(byPropKey('email', event.target.value))}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          value={passwordOne}
+          onChange={event => this.setState(byPropKey('passwordOne', event.target.value))}
+          type="password"
+          placeholder="Password"
+        />
+        <input
+          value={passwordTwo}
+          onChange={event => this.setState(byPropKey('passwordTwo', event.target.value))}
+          type="password"
+          placeholder="Confirm Password"
+        />
+        <button disabled={isInvalid} type="submit">
+          Sign Up
+        </button>
+
+        { error && <p>{error.message}</p> }
+      </form>
+    );
+  }
+}
+
+const SignUpLink = () => {
+  return (
+    <p>
+      Don't have an account?
+      {' '}
+      <Link to={routes.SIGN_UP}>Sign Up</Link>
+    </p>
+  );
+};
+
+export default withRouter(SignUpPage);
+
+export {
+  SignUpForm,
+  SignUpLink,
+};
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,15 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
+// const config = {
+//   apiKey: "AIzaSyCjulOvnGTKYWrmznnU70-W2NMKJgm8rMA",
+//   authDomain: "in-common-hackathon.firebaseapp.com",
+//   databaseURL: "https://in-common-hackathon.firebaseio.com",
+//   projectId: "in-common-hackathon",
+//   storageBucket: "in-common-hackathon.appspot.com",
+//   messagingSenderId: "964821891404"
+// };
+
 const config = {
   apiKey: "AIzaSyCjulOvnGTKYWrmznnU70-W2NMKJgm8rMA",
   authDomain: "in-common-hackathon.firebaseapp.com",
@@ -10,9 +19,9 @@ const config = {
   messagingSenderId: "964821891404"
 };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(config);
-}
+// if (!firebase.apps.length) {
+firebase.initializeApp(config);
+// }
 
 const auth = firebase.auth();
 
