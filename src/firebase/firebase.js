7deaa159699c4bc4023cede063f9557ebe9bbf74
jwaltrip <jake.waltrip@gmail.com>
2018-09-16 03:45:48 -0500

    added firebase configs

---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyCjulOvnGTKYWrmznnU70-W2NMKJgm8rMA",
+  authDomain: "in-common-hackathon.firebaseapp.com",
+  databaseURL: "https://in-common-hackathon.firebaseio.com",
+  projectId: "in-common-hackathon",
+  storageBucket: "in-common-hackathon.appspot.com",
+  messagingSenderId: "964821891404"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export {
+  auth
+};
